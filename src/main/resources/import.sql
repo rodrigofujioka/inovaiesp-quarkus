c8b8426b7805ab6a51c9dab1948cd23b307b3d80
rfujioka <rodrigofujioka@gmail.com>
2024-10-17 17:47:29 -0300

    INOVAOESP 2024.2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,35 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+-- Inserindo dados na tabela Product
+insert into product (id, name, price, manufactureDate, expirationDate, productType_id)
+values (1, 'Laptop', 1200.00, '2023-01-01', null, 1);
+
+insert into product (id, name, price, manufactureDate, expirationDate, productType_id)
+values (2, 'Smartphone', 800.00, '2023-02-01', null, 1);
+
+insert into product (id, name, price, manufactureDate, expirationDate, productType_id)
+values (3, 'T-shirt', 25.00, '2023-03-01', null, 3);
+
+-- Inserindo dados na tabela ProductType
+insert into producttype (id, typeName)
+values (1, 'ELECTRONICS');
+
+insert into producttype (id, typeName)
+values (2, 'PERISHABLE');
+
+insert into producttype (id, typeName)
+values (3, 'CLOTHING');
+
+-- Alterando a sequência de ID da tabela Product para continuar a partir do próximo valor
+alter sequence product_seq restart with 4;
+
+-- Inserindo dados na tabela Student
+insert into student (id, name, email, birthDate)
+values (1, 'John Doe', 'john.doe@example.com', '2000-01-01');
+
+insert into student (id, name, email, birthDate)
+values (2, 'Jane Smith', 'jane.smith@example.com', '1998-05-15');
+
+insert into student (id, name, email, birthDate)
+values (3, 'Alice Johnson', 'alice.johnson@example.com', '1995-10-10');
+
+-- Alterando a sequência de ID da tabela Student para continuar a partir do próximo valor
+alter sequence student_seq restart with 4;
